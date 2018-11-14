cc92a52627d71155aa9fcb7570dfa7e4f07ce78a
Femy Stephen <femystephen16@gmail.com>
2018-11-14 12:34:10 +0000

    upgraded django

--- django_blog/settings/dev.py
+++ django_blog/settings/dev.py
@@ -7,6 +7,8 @@ from .base import *
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT='staticfiles'
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
